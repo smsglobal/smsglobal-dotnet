ef4671d759ccacbae066a1dbf4ac890f6fe062dc
j-brooke <6962885+j-brooke@users.noreply.github.com>
2022-04-08 12:07:08 -0400

    Removed unnecessary and error-prone call to api.nuget.org before all calls to the SMSGlobal server.

--- SMSGlobal/Transport/Rest.cs
+++ SMSGlobal/Transport/Rest.cs
@@ -18,6 +18,7 @@ namespace SMSGlobal.SMS.Transport
     class Rest
     {
         private Uri uri;
+        private readonly string dotnetLibVersion;
 
         public string host { get; set; }
         public string port { get; set; }
@@ -32,6 +33,10 @@ namespace SMSGlobal.SMS.Transport
             version = _credentials.version;
             key = _credentials.ApiKey;
             secret = _credentials.ApiSecret;
+
+            // Figure out the version of this library, for use in the User-Agent header that we send.
+            var thisVersion = typeof(Rest).Assembly.GetName().Version;
+            dotnetLibVersion = $"{thisVersion.Major}.{thisVersion.Minor}.{thisVersion.Build}";
         }
 
         /// <summary>
@@ -338,7 +343,6 @@ namespace SMSGlobal.SMS.Transport
             return otp;
         }
 
-
         /// <summary>
         /// Requests information from the rest api.
         /// </summary>
@@ -368,16 +372,7 @@ namespace SMSGlobal.SMS.Transport
                 client.DefaultRequestHeaders.Accept.Clear();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("MAC", credentials);
-
-                // Get latest Nuget Package version.
-                var packageName = "SMSGlobal";
-                var url = $"https://api.nuget.org/v3-flatcontainer/{packageName}/index.json";
-                var httpClient = new HttpClient();
-                var responseNuget = await httpClient.GetAsync(url);
-                var versionsResponse = await responseNuget.Content.ReadAsAsync<VersionsResponse>();
-                var lastVersion = versionsResponse.Versions[^1]; //(length-1)
-
-                client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "SMSGlobal-SDK/v2 Version/" + lastVersion + ", DotNet/" + System.Environment.Version + " (" + Environment.OSVersion + ")");
+                client.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "SMSGlobal-SDK/v2 Version/" + dotnetLibVersion + ", DotNet/" + System.Environment.Version + " (" + Environment.OSVersion + ")");
 
                 var json = JsonConvert.SerializeObject(payload);
 
